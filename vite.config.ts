678346b2392d687c1ff5ce76d696338fd0e1ca53
Peyman Salimi <pmnslm6344@gmail.com>
2026-09-16 05:00:25 +0330

    chore: configure eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { fileURLToPath } from "node:url"
+import { fileURLToPath } from 'node:url';
 
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-const srcDir = fileURLToPath(new URL("./src", import.meta.url))
+const srcDir = fileURLToPath(new URL('./src', import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": srcDir,
+      '@': srcDir,
     },
   },
-})
+});
